96e5b682e0697900a15d7b56f32090497980621e
Richard Kersey <github@razorit.com>
2011-10-18 11:43:11 -0400

    More File and Folder Renaming, Got Rid of Credits Admin_

--- 0.0119.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE `projects` CHANGE `id` `id` VARCHAR( 36 ) NOT NULL ;
-
-ALTER TABLE `messages` CHANGE `id` `id` VARCHAR( 36 ) NULL DEFAULT NULL 
-
-ALTER TABLE `messages` CHANGE `parent_id` `parent_id` VARCHAR( 36 ) NULL DEFAULT NULL ;
-
---
